cce1fcca1f9f59e89f3a5031e05b53b8513ab8d6
amikun0509-rgb <amikun0509@gmail.com>
2025-10-10 18:04:23 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let submitted = false;
+
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("open");
+}
+
+// スライドショー
+const images = [
+  "images/S__25403487.jpg",
+  "images/S__25403488.jpg",
+  "images/S__25403497.jpg"
+];
+let current = 0;
+const slideshow = document.getElementById("slideshow");
+
+setInterval(() => {
+  current = (current + 1) % images.length;
+  slideshow.src = images[current];
+}, 3000); // 3秒ごとに切り替え
